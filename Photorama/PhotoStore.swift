9f13d041940f29502f7b8df3589f5e130b437bbe
inanfatih <fatih@users.noreply.github.com>
2018-03-12 22:37:50 -0400

    finished

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 
 enum PhotosResult {
     case success([Photo])
@@ -20,7 +30,7 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
     
-    func fetchInterestingPhotos () {
+    func fetchInterestingPhotos (completion: @escaping (PhotosResult) -> Void ) {
         
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
@@ -41,6 +51,9 @@ class PhotoStore {
             }
             
             let result = self.processPhotosRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -52,4 +65,40 @@ class PhotoStore {
         
         return FlickrAPI.photos(fromJson: jsonData)
     }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func processImageRequest (data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        
+        }
+        return .success(image)
+    }
+    
+    
+    
+    
+    
+    
 }
