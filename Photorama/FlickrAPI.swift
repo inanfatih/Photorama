468841d0d1bf44d90973dcd2fcd673957cd5d8a2
inanfatih <fatih@users.noreply.github.com>
2018-03-12 21:04:38 -0400

    slide 44 completed

--- Photorama/FlickrAPI.swift
+++ Photorama/FlickrAPI.swift
@@ -17,9 +17,36 @@ enum Method: String {
 struct FlickrAPI {
     
     private static let baseURLString = "https://api.flicker.com/services/rest"
-    
+    private static let apiKey = "bd04a3068a9101df2b9df4fc67822454"
     private static func flickrURL(method: Method, parameters: [String:String]?) -> URL {
-        return URL (string: "")!
+
+        var components =  URLComponents(string: baseURLString)!
+        
+        var queryItems = [URLQueryItem]()
+        
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback" : "1",
+            "api_key" : apiKey
+        ]
+        
+        for (key, value) in baseParams {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        
+        if let additionalParams = parameters{
+            for (key,value) in additionalParams {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        
+        components.queryItems = queryItems
+
+        return components.url!
     }
 
     static var interestingPhotosURL: URL {
--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        store.fetchInterestingPhotos()
+    }
 }
